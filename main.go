e424c5b22da70efc69f3c5a55174972c43b7f9fd
Jeroen Hoolmans <jhoolmans@gmail.com>
2017-10-31 00:12:14 +0100

    added doc for Engine.SetLocation

--- main.go
+++ main.go
@@ -28,6 +28,10 @@ type Engine struct {
 	configuration map[string]interface{}
 }
 
+/*SetLocation sets the Location and creates the proper reference.
+Not that this should be called _after_ the location has been added
+to the LocationStore to get the right reference link.
+*/
 func (e *Engine) SetLocation(l *Location) {
 	e.location = *l
 	e.LocationRef = "@" + l.Name()
