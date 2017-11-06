c39cf3a779092c3950a1189067c65416eb5780ba
Jeroen Hoolmans <jhoolmans@gmail.com>
2017-11-06 23:47:09 +0100

    Custom marshal code for engines.
    Flattened configurations map.

--- engine.go
+++ engine.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"encoding/json"
+)
+
 /*Engine represents a dcc application with shotgun configurations.
 Most importantly in a pipeline configuration this will also hold the
 source location of this engine.
@@ -34,4 +38,47 @@ to the LocationStore to get the right reference link.
 func (e *Engine) SetLocation(l *Location) {
 	e.location = *l
 	e.LocationRef = "@" + l.Name()
+}
+
+/*MarshalJSON override. This basically flattens the configuration to be
+on the same level as the other keys.
+*/
+func (e Engine) MarshalJSON() ([]byte, error) {
+	rawData := make(map[string]interface{})
+	rawData["location"] = e.LocationRef
+	rawData["apps"] = e.Apps
+
+	// loop through all configuration keys and add them to rawData
+	for k, v := range e.configuration {
+		rawData[k] = v
+	}
+	return json.Marshal(rawData)
+}
+
+/*UnmarshalJSON override. Reads all the data into a map[string]interface
+and sets anything that isn't location/apps into configuration.
+
+After unmarshal-ing to an Engine, the Engine won't have a valid Location
+reference object. In order to restore the connection, we have to relink
+it through an 'engine' LocationStore.
+*/
+func (e *Engine) UnmarshalJSON(b []byte) error {
+	data := make(map[string]interface{})
+	err := json.Unmarshal(b, &data)
+	if err != nil {
+		return err
+	}
+
+	if val, ok := data["apps"]; ok {
+		e.Apps = val.(map[string]interface{})
+		delete(data, "apps")
+	}
+	if val, ok := data["location"]; ok {
+		e.LocationRef = val.(string)
+		delete(data, "location")
+	}
+	for k, v := range data {
+		e.configuration[k] = v
+	}
+	return nil
 }
